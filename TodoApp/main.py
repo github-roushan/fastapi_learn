4e010adf827c4cc8a788ecdb18dbde904bfacc39
Roushan Singh <github.rtron18@gmail.com>
2025-07-25 18:32:17 +0530

    Add Basic Get and Post Method for Todo App

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -1,7 +1,52 @@
-from fastapi import FastAPI
-import models
-from database import engine
+from typing import Annotated
+from fastapi import FastAPI, Depends, Path, status, HTTPException
+from pydantic import BaseModel, Field
+from sqlalchemy.orm import Session
+from models import Todos, Base
+from database import engine, SessionLocal
 
 app = FastAPI()
 
-models.Base.metadata.create_all(bind = engine)
+Base.metadata.create_all(bind = engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+db_dependency = Annotated[Session, Depends(get_db)]
+class TodoRequest(BaseModel):
+    title: str
+    description: str
+    priority: int
+    complete: bool
+
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "title": "A sample todo",
+                "description": "What to do",
+                "priority": 5,
+                "complete": False
+            }
+        }
+
+@app.get("/")
+async def get_all_todos(db: db_dependency):
+    return db.query(Todos).all()
+
+@app.get("/todo/{todo_id}", status_code=status.HTTP_200_OK)
+async def get_todo_by_id(db: db_dependency, todo_id: int = Path(gt=0)):
+    todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+    if todo_model is not None:
+        return todo_model
+    raise HTTPException(status_code=404, detail=f"Todo With id: {todo_id} not found")
+
+@app.post("/create_todo", status_code=status.HTTP_200_OK)
+async def create_todo(todo: TodoRequest, db: db_dependency):
+    todo = Todos(**todo.model_dump())
+    db.add(todo)
+    db.commit()
+    return {"message": "Added a todo"}

f60796ae4ccf29f4cc25798cbec0fe614b348759
Roushan Singh <github.rtron18@gmail.com>
2025-07-28 19:02:56 +0530

    Add A Section for Todo in Swagger and Use Tokens to create todos

--- TodoApp/routers/todos.py
+++ TodoApp/routers/todos.py
@@ -2,8 +2,12 @@ from fastapi import APIRouter, Path, status, HTTPException
 from pydantic import BaseModel, Field
 from models import Todos
 from utils import db_dependency
+from .auth import user_dependency
 
-router = APIRouter()
+router = APIRouter(
+    prefix = "/todo",
+    tags = ["Todo"]
+)
 
 class TodoRequest(BaseModel):
     title: str = Field(min_length=1)
@@ -21,36 +25,36 @@ class TodoRequest(BaseModel):
             }
         }
 
-@router.get("/")
+@router.get("/all")
 async def get_all_todos(db: db_dependency):
     return db.query(Todos).all()
 
-@router.get("/todo/{todo_id}", status_code=status.HTTP_200_OK)
+@router.get("/{todo_id}", status_code=status.HTTP_200_OK)
 async def get_todo_by_id(db: db_dependency, todo_id: int = Path(gt=0)):
     todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
     if todo_model is not None:
         return todo_model
     raise HTTPException(status_code=404, detail=f"Todo With id: {todo_id} not found")
 
-@router.post("/create_todo", status_code=status.HTTP_201_CREATED)
-async def create_todo(todo_model: TodoRequest, db: db_dependency):
-    todo_model = Todos(**todo_model.model_dump())
+@router.post("/create", status_code=status.HTTP_201_CREATED)
+async def create_todo(todo_model: TodoRequest, db: db_dependency, user: user_dependency):
+    todo_model = Todos(**todo_model.model_dump(), owner_id = user.get('id'))
     db.add(todo_model)
     db.commit()
-    return {"message": "Added a todo"}
+    return {"detail": "Added a todo"}
 
-@router.put("/todo/{todo_id}", status_code=status.HTTP_204_NO_CONTENT)
+@router.put("/{todo_id}", status_code=status.HTTP_204_NO_CONTENT)
 async def update_todo(db: db_dependency, todo_request: TodoRequest, todo_id: int = Path(gt=0)):
     todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
     if todo_model is None:
         raise HTTPException(status_code=404, detail=f"No Such Todo with id: {todo_id}")
-    
+
     for key, value in todo_request.dict().items():
         setattr(todo_model, key, value)
 
     db.commit()
 
-@router.delete("/todo/{todo_id}", status_code=status.HTTP_204_NO_CONTENT)
+@router.delete("/{todo_id}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_todo(db: db_dependency, todo_id: int = Path(gt=0)):
     query_result = db.query(Todos).filter(Todos.id == todo_id).delete()
     if query_result == 0:

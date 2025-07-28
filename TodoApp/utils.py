f96369917a00ed5242fcc474fd8dde05859922ac
Roushan Singh <github.rtron18@gmail.com>
2025-07-28 15:45:35 +0530

    Add Password hashing utility func in utils.py

--- TodoApp/utils.py
+++ TodoApp/utils.py
@@ -1,9 +1,8 @@
 from typing import Annotated
-
 from fastapi import Depends
 from requests import Session
 from database import SessionLocal
-
+from passlib.context import CryptContext
 
 def get_db():
     db = SessionLocal()
@@ -13,3 +12,6 @@ def get_db():
         db.close()
 
 db_dependency = Annotated[Session, Depends(get_db)]
+
+## password hashing utility
+bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated='auto')

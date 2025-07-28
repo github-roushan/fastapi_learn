74229e13c4722a9d12faa5ed244cb7790cbd36a3
Roushan Singh <github.rtron18@gmail.com>
2025-07-28 19:32:32 +0530

    Add Object Representation to Models

--- TodoApp/models.py
+++ TodoApp/models.py
@@ -1,5 +1,7 @@
 from database import Base
 from sqlalchemy import Column, ForeignKey, Integer, String, Boolean
+import json
+
 
 class Users(Base):
     __tablename__ = 'users'
@@ -9,8 +11,23 @@ class Users(Base):
     first_name = Column(String)
     last_name = Column(String)
     hashed_password = Column(String)
-    is_active = Column(Boolean, default = True)
+    is_active = Column(Boolean, default=True)
     role = Column(String)
+
+    def __repr__(self):
+        # Create a dictionary of the object attributes
+        user_dict = {
+            'id': self.id,
+            'username': self.username,
+            'email': self.email,
+            'first_name': self.first_name,
+            'last_name': self.last_name,
+            'is_active': self.is_active,
+            'role': self.role
+        }
+
+        # `default=str` handles non-serializable types
+        return json.dumps(user_dict, default=str)
 
 class Todos(Base):
     __tablename__ = 'todos'
@@ -21,3 +38,17 @@ class Todos(Base):
     priority = Column(Integer)
     complete = Column(Boolean, default=False)
     owner_id = Column(Integer, ForeignKey("users.id"))
+
+    def __repr__(self):
+        # Create a dictionary of the object attributes
+        todo_dict = {
+            'id': self.id,
+            'title': self.title,
+            'description': self.description,
+            'priority': self.priority,
+            'complete': self.complete,
+            'owner_id': self.owner_id
+        }
+
+        # `default=str` handles non-serializable types
+        return json.dumps(todo_dict, default=str)

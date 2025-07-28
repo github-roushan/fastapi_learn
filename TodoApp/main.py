1c6242c63fbd3ca9f0430eea384973ba94e0cca1
Roushan Singh <github.rtron18@gmail.com>
2025-07-28 23:02:37 +0530

    Add admin routes in main.py file

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
 from models import Base
 from database import engine
-from routers import auth, todos
+from routers import auth, todos, admin
 
 app = FastAPI()
 
 Base.metadata.create_all(bind = engine)
+app.include_router(admin.router)
 app.include_router(auth.router)
 app.include_router(todos.router)

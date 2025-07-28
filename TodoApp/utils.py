66024bad8071b2e8d913cc0f4bf9c4486ffe14c4
Roushan Singh <github.rtron18@gmail.com>
2025-07-28 22:48:37 +0530

    Add a decorator for is_admin

--- TodoApp/utils.py
+++ TodoApp/utils.py
@@ -1,8 +1,9 @@
 from typing import Annotated
-from fastapi import Depends
+from fastapi import Depends, HTTPException, status
 from requests import Session
 from database import SessionLocal
 from passlib.context import CryptContext
+from functools import wraps
 
 def get_db():
     db = SessionLocal()
@@ -15,3 +16,15 @@ db_dependency = Annotated[Session, Depends(get_db)]
 
 ## password hashing utility
 bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated='auto')
+
+## Decorator to check for admin role
+def is_admin(func):
+    @wraps(func)
+    async def wrapper(user, db, *args, **kwargs):
+        if user is None or user.get("role") != "admin":
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail = "Not an Admin"
+            )
+        return await func(user, db, *args, **kwargs)
+    return wrapper

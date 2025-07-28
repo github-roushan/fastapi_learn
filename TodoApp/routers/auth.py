d0e68fe568a731bdbabcf598f58f984c655e21ff
Roushan Singh <github.rtron18@gmail.com>
2025-07-28 19:01:39 +0530

    Add Code to create users, generate tokens and validate tokens

--- TodoApp/routers/auth.py
+++ TodoApp/routers/auth.py
@@ -1,11 +1,32 @@
-from fastapi import APIRouter, HTTPException, status
+from datetime import datetime, timedelta, timezone
+from typing import Annotated
+from fastapi import APIRouter, Depends, HTTPException, status
 from pydantic import BaseModel
 from models import Users
 from hashlib import md5
 from utils import db_dependency, bcrypt_context
+from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
+from enum import Enum
+from jose import JWTError, jwt
+import os
+from dotenv import load_dotenv
 
 
-router = APIRouter()
+router = APIRouter(
+    prefix = "/auth",
+    tags = ["Auth"]
+)
+
+load_dotenv()
+SECRET_KEY = os.getenv('SECRET_KEY')
+ALGORITHM = os.getenv("ALGORITHM")
+oauth2_bearer = OAuth2PasswordBearer(tokenUrl="auth/token/generate")
+
+class RoleEnum(str, Enum):
+    ten_x = "10x"
+    admin = "admin"
+    prime = "prime"
+    normal = "normal"
 
 class CreateUserRequest(BaseModel):
     username: str
@@ -13,7 +34,7 @@ class CreateUserRequest(BaseModel):
     first_name: str
     last_name: str
     password: str
-    role: str
+    role: RoleEnum
 
     class Config:
         json_schema_extra = {
@@ -27,7 +48,42 @@ class CreateUserRequest(BaseModel):
             }
         }
 
-@router.post("/auth", status_code=status.HTTP_201_CREATED)
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+def authenticate_user(username: str, password: str, db):
+    if ( user := db.query(Users).filter(Users.username == username).first()):
+        if bcrypt_context.verify(password, user.hashed_password):
+            return True,user
+    return False, None
+
+def create_access_token(username: str, user_id: int, time_to_live: timedelta):
+    encode = {"sub": username, 'id': user_id}
+    expires = datetime.now(timezone.utc) + time_to_live
+    encode['exp'] = expires
+    return jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_bearer)]):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        user_id: int = payload.get("id")
+        if username is None or user_id is None:
+            raise HTTPException(
+                status_code = status.HTTP_401_UNAUTHORIZED,
+                detail = "Could Not Validate User"
+            )
+        return {"username": username, "id": user_id}
+    except JWTError:
+        raise HTTPException(
+                status_code = status.HTTP_401_UNAUTHORIZED,
+                detail = "Could Not Validate User"
+            )
+    
+user_dependency = Annotated[dict, Depends(get_current_user)]
+
+@router.post("/create", status_code=status.HTTP_201_CREATED)
 async def create_user(create_user_req: CreateUserRequest, db: db_dependency):
     if (existingUserEmail := db.query(Users).filter(Users.email == create_user_req.email).first()):
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Email Already Registered")
@@ -41,4 +97,15 @@ async def create_user(create_user_req: CreateUserRequest, db: db_dependency):
     db.add(create_user_model)
     db.commit()
 
-    return {"message": "User Successfully created"}
+    return {"detail": "User Successfully created"}
+
+@router.post("/token/generate", status_code=status.HTTP_200_OK, response_model=Token)
+async def generate_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: db_dependency):
+    is_authenticated, user = authenticate_user(form_data.username, form_data.password, db)
+    if is_authenticated:
+        token = create_access_token(user.username, user.id, timedelta(minutes=30))
+        return {"access_token": token, "token_type":"Bearer"}
+    raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail = "Invalid Username or password"
+    )

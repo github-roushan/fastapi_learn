dd9a14abd930a253dd194d549c9a9a5097f3e83f
Roushan Singh <github.rtron18@gmail.com>
2025-07-31 18:24:49 +0530

    Add Code to connect to pg database now

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -1,12 +1,16 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from settings import BASE_DIR, POSTGRES_DB, POSTGRES_PASS, POSTGRES_USER
 
-import pathlib
-BASE_DIR = pathlib.Path(__file__).parent
-SQLALCHEMY_DATABASE_URL = f'sqlite:///{BASE_DIR}/todosapp.db'
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
+## For Sqlite Database
+# SQLALCHEMY_SQLITE_DATABASE_URL = f'sqlite:///{BASE_DIR}/todosapp.db'
+# engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
+
+## For Postgres Database
+SQLALCHEMY_PG_DATABASE_URL = f'postgresql://{POSTGRES_USER}:{POSTGRES_PASS}@localhost/{POSTGRES_DB}'
+engine = create_engine(SQLALCHEMY_PG_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind = engine)
 

4f92b324ba824ed3badcb13c01ac82d16375f70f
Roushan Singh <github.rtron18@gmail.com>
2025-07-31 18:49:15 +0530

    Fix Sqlite DB variable

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -4,9 +4,9 @@ from sqlalchemy.ext.declarative import declarative_base
 from settings import BASE_DIR, POSTGRES_DB, POSTGRES_PASS, POSTGRES_USER
 
 
-## For Sqlite Database
+# # For Sqlite Database
 # SQLALCHEMY_SQLITE_DATABASE_URL = f'sqlite:///{BASE_DIR}/todosapp.db'
-# engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
+# engine = create_engine(SQLALCHEMY_SQLITE_DATABASE_URL, connect_args={'check_same_thread': False})
 
 ## For Postgres Database
 SQLALCHEMY_PG_DATABASE_URL = f'postgresql://{POSTGRES_USER}:{POSTGRES_PASS}@localhost/{POSTGRES_DB}'

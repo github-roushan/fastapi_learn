dcdead897566f3bee64e64d6463d8baec26bc0ce
Roushan Singh <github.rtron18@gmail.com>
2025-07-31 18:23:34 +0530

    Move load_dotenv here and read pg variables

--- TodoApp/settings.py
+++ TodoApp/settings.py
@@ -1,3 +1,11 @@
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
 BASE_DIR = Path(__file__).parent
+
+load_dotenv(dotenv_path=f"{BASE_DIR}/.env")
+
+POSTGRES_PASS = os.getenv('POSTGRES_PASS')
+POSTGRES_USER = os.getenv('POSTGRES_USER')
+POSTGRES_DB = os.getenv('POSTGRES_DB')
